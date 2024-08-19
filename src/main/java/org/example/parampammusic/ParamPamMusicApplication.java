722202e50a141104edc5b71a0922464d40bdce69
Kirill Palianitsa <kirillpal22@gmail.com>
2024-08-19 19:26:44 +0300

    Create project

---
+++ src/main/java/org/example/parampammusic/ParamPamMusicApplication.java
@@ -0,0 +1,13 @@
+package org.example.parampammusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamPamMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParamPamMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parampammusic/ParamPamMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parampammusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamPamMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
